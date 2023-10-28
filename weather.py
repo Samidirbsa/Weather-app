a36496460a7bdf184ea25dcdbe1c47560ae4929e
Sami <samuel47dribsa@gmail.com>
2023-10-27 18:14:01 -0700

    create weather data from api

---
+++ weather.py
@@ -0,0 +1,7 @@
+import requests
+
+def get_weather_data(city):
+  url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY"
+  response = requests.get(url)
+  data = response.json()
+  return data
