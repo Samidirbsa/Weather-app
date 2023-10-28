e210e142857d416f37b717597bd6f1844748155d
Sami <samuel47dribsa@gmail.com>
2023-10-27 18:21:36 -0700

    add response request

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
+#weather forcast
 import requests
-
 def get_weather_data(city):
   url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY"
   response = requests.get(url)
-  data = response.json()
+  data = response.json
   return data
